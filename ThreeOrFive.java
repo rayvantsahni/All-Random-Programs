f86cf83c2427ad08fb282555189d72fa4deada46
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-16 02:13:30 +0530

    Create ThreeOrFive.java

---
+++ ThreeOrFive.java
@@ -0,0 +1,16 @@
+public class ThreeOrFive
+{
+  public static void main(String[] args)
+  {
+    for (int i = 1; i <=100; i++)
+    {
+      if (i % 3 == 0)
+      {
+        if (i % 5 == 0) System.out.println("Both");
+        else System.out.println("Three");
+      }
+      else if (i % 5 == 0) System.out.println("Five");
+      else System.out.println(i);
+    }
+  }
+}
