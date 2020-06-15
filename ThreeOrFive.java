4ca7be1b1ce308e43f52c916cbcc03e3e33f2788
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-16 02:14:09 +0530

    Update ThreeOrFive.java

--- ThreeOrFive.java
+++ ThreeOrFive.java
@@ -2,7 +2,7 @@ public class ThreeOrFive
 {
   public static void main(String[] args)
   {
-    for (int i = 1; i <=100; i++)
+    for (int i = 1; i <= 100; i++)
     {
       if (i % 3 == 0)
       {
