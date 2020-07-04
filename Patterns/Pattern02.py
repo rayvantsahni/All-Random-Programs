2934fb62527389cf30ef2e61e976a38b7ec4d1d3
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-05 01:11:48 +0530

    Create Pattern02.py

---
+++ Patterns/Pattern02.py
@@ -0,0 +1,22 @@
+n = int(input("Enter any odd number: "))
+print()
+
+for i in range(1, n//2+2):
+    for j in range(1, n+1):
+        if i == j or j == n - i + 1:
+            print(j, end = "")
+        else:
+            print(" ", end = "")
+            
+    print()
+    
+'''
+1             15
+ 2           14 
+  3         13  
+   4       12   
+    5     11    
+     6   10     
+      7 9      
+       8 
+'''
