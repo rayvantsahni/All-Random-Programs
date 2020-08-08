b79dc91bfb0d5da1a2f586ba7cfc90a1285fa602
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-09 00:54:12 +0530

    Create is_palindrome.cpp

---
+++ Is Palindrome?/is_palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool is_palindrome(string text) 
+{
+  string reversed_text = "";
+  
+  for (int i = text.size() - 1; i >= 0; i--) 
+  {
+    reversed_text += text[i];
+  }
+  
+  if (reversed_text == text) 
+  {
+    return true;
+  }
+  
+  return false; 
+}
+
+int main() 
+{  
+  cout << is_palindrome("madam") << "\n";
+  cout << is_palindrome("ada") << "\n";
+  cout << is_palindrome("lovelace") << "\n";
+}
